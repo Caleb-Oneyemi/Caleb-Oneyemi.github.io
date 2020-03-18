6c3023f57c7874854ad5ecff52f2d90f1aaada84
Caleb <caloneyemi@gmail.com>
2020-03-18 21:40:51 +0100

    update

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Tony Stark',
+	username: 'Iron Man',
+	email: 'tonystark@gmail.com' 
+},
+{
+	id: 2,
+	name: 'King Tchalla',
+	username: 'Black Panther',
+	email: 'tchalla@gmail.com' 
+},
+{
+	id: 3,
+	name: 'Steve Rogers',
+	username: 'Captain America',
+	email: 'steverogers@gmail.com' 
+},
+{
+	id: 4,
+	name: 'Natasha Romanov',
+	username: 'Black Widow',
+	email: 'natasharomanov@gmail.com' 
+},
+{
+	id: 5,
+	name: 'Thor Odinson',
+	username: 'Thor',
+	email: 'thor@gmail.com' 
+},
+{
+	id: 6,
+	name: 'Bruce Banner',
+	username: 'Hulk',
+	email: 'brucebanner@gmail.com' 
+},
+{
+	id: 7,
+	name: 'Steven Strange',
+	username: 'Dr Strange',
+	email: 'stevenstrange@gmail.com' 
+},
+{
+	id: 8,
+	name: 'Carol Danvers',
+	username: 'Captain Marvel',
+	email: 'caroldanvers@gmail.com' 
+},
+{
+	id: 9,
+	name: 'Peter Parker',
+	username: 'Spider Man',
+	email: 'peterparker@gmail.com' 
+},
+{
+	id: 10,
+	name: 'Loki Odinson',
+	username: 'Loki',
+	email: 'lokiodinson@gmail.com' 
+}
+]
